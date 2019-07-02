b58b90bfa30c1983302f40d497e41af8e3f04be4
martin431033 <52283917+martin431033@users.noreply.github.com>
2019-07-01 22:41:53 -0700

    Update economy.js

--- economy.js
+++ economy.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
 
-
+//hi
 let prefix = config.prefix;
 client.on('message', message => {
 
